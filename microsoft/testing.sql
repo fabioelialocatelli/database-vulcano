369018bceb1932c546fae213e298f6538cf03da4
Fabio Locatelli [DATACOM] <FabioL@datacom.co.nz>
2018-02-28 14:14:16 +1300

    Triggers Ported
    All triggers have been ported and tested. A pesky bug associated with date formatting has been found and fixed.

--- microsoft/testing.sql
+++ microsoft/testing.sql
@@ -9,3 +9,39 @@ GO
 
 INSERT INTO volcanocaldera(volcanoIdentifier, localName) VALUES('CMP', 'Campi Flegrei')
 GO
+
+UPDATE volcanoCaldera SET localName = 'CampiFlegrei' WHERE volcanoIdentifier = 'CMP'
+GO
+
+UPDATE volcanoCaldera SET localName = 'Campi Flegrei' WHERE volcanoIdentifier = 'CMP'
+GO
+
+TRUNCATE TABLE historyActive
+GO
+
+DELETE FROM volcanoActive WHERE volcanoIdentifier = 'SFR'
+GO
+
+INSERT INTO volcanoActive(volcanoIdentifier, localName) VALUES('SFR', 'Soufrière Hills')
+GO
+
+UPDATE volcanoActive SET localName = 'SoufrièreHills' WHERE volcanoIdentifier = 'SFR'
+GO
+
+UPDATE volcanoActive SET localName = 'Soufrière Hills' WHERE volcanoIdentifier = 'SFR'
+GO
+
+TRUNCATE TABLE historyInactive
+GO
+
+DELETE FROM volcanoInactive WHERE volcanoIdentifier = 'MKS'
+GO
+
+INSERT INTO volcanoInactive(volcanoIdentifier, localName) VALUES('MKS', 'Emi Koussi')
+GO
+
+UPDATE volcanoInactive SET localName = 'EmiKoussi' WHERE volcanoIdentifier = 'MKS'
+GO
+
+UPDATE volcanoInactive SET localName = 'Emi Koussi' WHERE volcanoIdentifier = 'MKS'
+GO
--- microsoft/triggers.sql
+++ microsoft/triggers.sql
@@ -8,13 +8,13 @@ CREATE TRIGGER calderaAdded
 	ON volcanoCaldera
 		AFTER INSERT AS
 			BEGIN
-				DECLARE @timestampSystem       AS DATETIME = GETDATE()
-				DECLARE @timestampFormatting   AS INT = 100
-				DECLARE @timestampFormatted    AS VARCHAR(30) = CONVERT(VARCHAR(20), @timestampSystem, @timestampFormatting)
-				DECLARE @newCaldera            AS VARCHAR(45) = (SELECT localName FROM inserted)
-				DECLARE @newCalderaFormatted   AS VARCHAR(45) = LOWER(REPLACE(@newCaldera, ' ', ''))
-				DECLARE @volcanoIdentifier     AS VARCHAR(45) = (SELECT volcanoIdentifier FROM inserted)
-				DECLARE @operationIdentifier   AS VARCHAR(95) = @timestampFormatted
+				DECLARE @timestampSystem		AS DATETIME = GETDATE()
+				DECLARE @timestampFormatting	AS INT = 21
+				DECLARE @timestampFormatted		AS VARCHAR(23) = CONVERT(VARCHAR(23), @timestampSystem, @timestampFormatting)
+				DECLARE @newCaldera				AS VARCHAR(45) = (SELECT localName FROM inserted)
+				DECLARE @newCalderaFormatted	AS VARCHAR(45) = LOWER(REPLACE(@newCaldera, ' ', ''))
+				DECLARE @volcanoIdentifier		AS VARCHAR(45) = (SELECT volcanoIdentifier FROM inserted)
+				DECLARE @operationIdentifier	AS VARCHAR(95) = @timestampFormatted
 					+ ' added '
 					+ @newCalderaFormatted
 				BEGIN
@@ -29,6 +29,181 @@ CREATE TRIGGER calderaAdded
 						@timestampFormatted,
 						'INSERTION',
 						@newCaldera,
+						@volcanoIdentifier
+					)
+
+				END
+			END
+GO
+
+DROP TRIGGER IF EXISTS calderaEdited
+GO
+
+CREATE TRIGGER calderaEdited 
+	ON volcanoCaldera
+		FOR UPDATE AS
+			BEGIN
+				DECLARE @timestampSystem			AS DATETIME = GETDATE()
+				DECLARE @timestampFormatting		AS INT = 21
+				DECLARE @timestampFormatted			AS VARCHAR(23) = CONVERT(VARCHAR(23), @timestampSystem, @timestampFormatting)
+				DECLARE @existingCaldera			AS VARCHAR(45) = (SELECT localName FROM inserted)
+				DECLARE @existingCalderaFormatted	AS VARCHAR(45) = LOWER(REPLACE(@existingCaldera, ' ', ''))
+				DECLARE @volcanoIdentifier			AS VARCHAR(45) = (SELECT volcanoIdentifier FROM inserted)
+				DECLARE @operationIdentifier		AS VARCHAR(95) = @timestampFormatted
+					+ ' edited '
+					+ @existingCalderaFormatted
+				BEGIN
+					INSERT INTO historyCaldera (
+						operationIdentifier,
+						operationTime,
+						operationType,
+						localName,
+						volcanoIdentifier
+					) VALUES (
+						@operationIdentifier,
+						@timestampFormatted,
+						'EDITION',
+						@existingCaldera,
+						@volcanoIdentifier
+					)
+
+				END
+			END
+GO
+
+DROP TRIGGER IF EXISTS activeAdded
+GO
+
+CREATE TRIGGER activeAdded 
+	ON volcanoActive
+		AFTER INSERT AS
+			BEGIN
+				DECLARE @timestampSystem		AS DATETIME = GETDATE()
+				DECLARE @timestampFormatting	AS INT = 21
+				DECLARE @timestampFormatted		AS VARCHAR(23) = CONVERT(VARCHAR(23), @timestampSystem, @timestampFormatting)
+				DECLARE @newActive				AS VARCHAR(45) = (SELECT localName FROM inserted)
+				DECLARE @newActiveFormatted		AS VARCHAR(45) = LOWER(REPLACE(@newActive, ' ', ''))
+				DECLARE @volcanoIdentifier		AS VARCHAR(45) = (SELECT volcanoIdentifier FROM inserted)
+				DECLARE @operationIdentifier	AS VARCHAR(95) = @timestampFormatted
+					+ ' added '
+					+ @newActiveFormatted
+				BEGIN
+					INSERT INTO historyActive(
+						operationIdentifier,
+						operationTime,
+						operationType,
+						localName,
+						volcanoIdentifier
+					) VALUES (
+						@operationIdentifier,
+						@timestampFormatted,
+						'INSERTION',
+						@newActive,
+						@volcanoIdentifier
+					)
+
+				END
+			END
+GO
+
+DROP TRIGGER IF EXISTS activeEdited
+GO
+
+CREATE TRIGGER activeEdited 
+	ON volcanoActive
+		FOR UPDATE AS
+			BEGIN
+				DECLARE @timestampSystem			AS DATETIME = GETDATE()
+				DECLARE @timestampFormatting		AS INT = 21
+				DECLARE @timestampFormatted			AS VARCHAR(23) = CONVERT(VARCHAR(23), @timestampSystem, @timestampFormatting)
+				DECLARE @existingActive				AS VARCHAR(45) = (SELECT localName FROM inserted)
+				DECLARE @existingActiveFormatted	AS VARCHAR(45) = LOWER(REPLACE(@existingActive, ' ', ''))
+				DECLARE @volcanoIdentifier			AS VARCHAR(45) = (SELECT volcanoIdentifier FROM inserted)
+				DECLARE @operationIdentifier		AS VARCHAR(95) = @timestampFormatted
+					+ ' edited '
+					+ @existingActiveFormatted
+				BEGIN
+					INSERT INTO historyActive (
+						operationIdentifier,
+						operationTime,
+						operationType,
+						localName,
+						volcanoIdentifier
+					) VALUES (
+						@operationIdentifier,
+						@timestampFormatted,
+						'EDITION',
+						@existingActive,
+						@volcanoIdentifier
+					)
+
+				END
+			END
+GO
+
+DROP TRIGGER IF EXISTS inactiveAdded
+GO
+
+CREATE TRIGGER inactiveAdded 
+	ON volcanoInactive
+		AFTER INSERT AS
+			BEGIN
+				DECLARE @timestampSystem		AS DATETIME = GETDATE()
+				DECLARE @timestampFormatting	AS INT = 21
+				DECLARE @timestampFormatted		AS VARCHAR(23) = CONVERT(VARCHAR(23), @timestampSystem, @timestampFormatting)
+				DECLARE @newInactive			AS VARCHAR(45) = (SELECT localName FROM inserted)
+				DECLARE @newInactiveFormatted	AS VARCHAR(45) = LOWER(REPLACE(@newInactive, ' ', ''))
+				DECLARE @volcanoIdentifier		AS VARCHAR(45) = (SELECT volcanoIdentifier FROM inserted)
+				DECLARE @operationIdentifier	AS VARCHAR(95) = @timestampFormatted
+					+ ' added '
+					+ @newInactiveFormatted
+				BEGIN
+					INSERT INTO historyInactive(
+						operationIdentifier,
+						operationTime,
+						operationType,
+						localName,
+						volcanoIdentifier
+					) VALUES (
+						@operationIdentifier,
+						@timestampFormatted,
+						'INSERTION',
+						@newInactive,
+						@volcanoIdentifier
+					)
+
+				END
+			END
+GO
+
+DROP TRIGGER IF EXISTS inactiveEdited
+GO
+
+CREATE TRIGGER inactiveEdited 
+	ON volcanoInactive
+		FOR UPDATE AS
+			BEGIN
+				DECLARE @timestampSystem			AS DATETIME = GETDATE()
+				DECLARE @timestampFormatting		AS INT = 21
+				DECLARE @timestampFormatted			AS VARCHAR(23) = CONVERT(VARCHAR(23), @timestampSystem, @timestampFormatting)
+				DECLARE @existingInactive			AS VARCHAR(45) = (SELECT localName FROM inserted)
+				DECLARE @existingInactiveFormatted	AS VARCHAR(45) = LOWER(REPLACE(@existingInactive, ' ', ''))
+				DECLARE @volcanoIdentifier			AS VARCHAR(45) = (SELECT volcanoIdentifier FROM inserted)
+				DECLARE @operationIdentifier		AS VARCHAR(95) = @timestampFormatted
+					+ ' edited '
+					+ @existingInactiveFormatted
+				BEGIN
+					INSERT INTO historyInactive (
+						operationIdentifier,
+						operationTime,
+						operationType,
+						localName,
+						volcanoIdentifier
+					) VALUES (
+						@operationIdentifier,
+						@timestampFormatted,
+						'EDITION',
+						@existingInactive,
 						@volcanoIdentifier
 					)
 
