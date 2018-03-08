bceae259a346daac269e9f15fb5e029aca5a23c7
Fabio Elia Locatelli <fabioelialocatelli@yandex.com>
2018-03-08 14:43:26 +1300

    More Volcanic Functions
    The two functions derived from "Encyclopedia of Volcanoes" have been successfully (and finally) ported to MySQL.

---
+++ mysql/functions.sql
@@ -0,0 +1,23 @@
+USE vulcano;
+
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS eruptionMagnitude$$
+CREATE FUNCTION eruptionMagnitude(eruptedMass FLOAT) RETURNS FLOAT
+SQL SECURITY INVOKER
+BEGIN
+	DECLARE magnitude FLOAT;
+    SET magnitude := LOG(10, @eruptedMass) - 7;
+	RETURN(magnitude);
+END$$
+
+DROP FUNCTION IF EXISTS eruptionIntensity$$
+CREATE FUNCTION eruptionIntensity(massEruptionRate FLOAT) RETURNS FLOAT
+SQL SECURITY INVOKER
+BEGIN
+	DECLARE intensity FLOAT;
+    SET intensity := LOG(10, @massEruptionRate) + 3;
+	RETURN(intensity);
+END$$
+
+DELIMITER ;
