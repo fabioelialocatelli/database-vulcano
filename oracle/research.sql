57206f31a0d55d8a85b3a098091c5f1ae06ea112
Fabio Elia Locatelli <fabioelialocatelli@yandex.com>
2018-03-09 16:59:17 +1300

    Oracle Research Expansion
    Research is being conducted on the Oracle platform. At this stage time functions are being analysed.

--- oracle/research.sql
+++ oracle/research.sql
@@ -4,11 +4,37 @@ FROM
     dual;
 
 SELECT
-    COALESCE(NULL,'Deccan Traps', NULL,'Pinatubo')
+    COALESCE(NULL,'Deccan Traps',NULL,'Pinatubo')
 FROM
     dual;
 
 SELECT
-    NVL(TO_CHAR(isDecade), 'Gone Missing')
+    NVL(TO_CHAR(isDecade),'Gone Missing')
 FROM
     volcanoActive;
+
+SELECT
+    CURRENT_DATE
+FROM
+    dual;
+
+SELECT
+    CURRENT_TIMESTAMP,
+    LOCALTIMESTAMP
+FROM
+    dual;
+
+SELECT
+    TO_CHAR(LOCALTIMESTAMP,'YYYY/MM/DD HH24:MM:SSxFF')
+FROM
+    dual;
+
+SELECT
+    CURRENT_TIMESTAMP - NUMTODSINTERVAL(100,'DAY')
+FROM
+    dual;
+
+SELECT
+    REGEXP_REPLACE(TO_CHAR(LOCALTIMESTAMP,'YYYYMMDDHH24MMSSFF'),'\s')
+FROM
+    dual;
